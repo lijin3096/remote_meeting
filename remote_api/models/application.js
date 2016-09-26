60b524fb44ae76a8a613e6d29187b5bf7d979fe6
Rui Liu <liurui81@gmail.com>
2016-09-26 21:53:36 +0800

    test

--- remote_api/models/application.js
+++ remote_api/models/application.js
@@ -182,8 +182,8 @@ Application.prototype.search = function(query, cb) {
   
   Logger.debug(condition);
   Logger.debug(condition2);
-  this.model.find({ orgCode: query.orgCode, 
-                   'applyHistory.applyDate': {'$gte': '2016-08-31', '$lte': '2016-08-31'}
+  this.model.find({ orgCode: query.orgCode
+                   //'applyHistory.applyDate': {'$gte': '2016-08-31', '$lte': '2016-08-31'}
                   },
      (err, applications) => {
       if (err) {
