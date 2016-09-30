fde1add11e2f94b8d6731b11ac318075c72ab6db
Rui Liu <liurui81@gmail.com>
2016-09-30 14:21:01 +0800

    refactor User model

--- remote_api/models/user.js
+++ remote_api/models/user.js
@@ -6,16 +6,20 @@ const mongoose  = require('../db');
 const ObjectId  = mongoose.Types.ObjectId;
 const Utils     = require('./../utils/utils');
 
-const UserSchema = new mongoose.Schema({
-  userid:           String,
-  name:             String,
-  phone:            { type:String, default: 'undefined' }, 
-  hashedPassword:   String,
-  orgnization:      { code: String, title: String },
-  cloudMsg:         { cloudID: String, token: String },
-  shorts:           [String]
+function User() {
+  this.schema = new mongoose.Schema({
+    userid:           String,
+    name:             String,
+    phone:            { type:String, default: 'undefined' }, 
+    hashedPassword:   String,
+    orgnization:      { code: String, title: String },
+    cloudMsg:         { cloudID: String, token: String },
+    shorts:           [String]
+  });
 
-});
+  this.model = mongoose.model('User', schema);
+}
+
 
 /**
  * Validation user for app login.
@@ -25,8 +29,8 @@ const UserSchema = new mongoose.Schema({
  * result contains user's information except password.
  * 
 */
-UserSchema.statics.isValidUser = function(userid, password, cb) {
-  this.findOne({userid: userid}).then((user) => {
+User.prototype.isValidUser = function(userid, password, cb) {
+  this.model.findOne({userid: userid}).then( (user) => {
     if (user) 
       bcrypt.compare(password, user.hashedPassword, (err, res) => {
         if(err) {
@@ -50,7 +54,9 @@ UserSchema.statics.isValidUser = function(userid, password, cb) {
       });
     else 
       return cb(null, {valid: false, user: 'not found'});
-  }).catch((err) => { cb(null, {valid: false, user: `found user ${err}`}); });
+  }).catch( (err) => { 
+    cb(null, {valid: false, user: `found user ${err}`}); 
+  });
 };
 
 
@@ -59,10 +65,10 @@ UserSchema.statics.isValidUser = function(userid, password, cb) {
  * @param {!User} user instance.
  * @param {function(?error)} callback.
 */
-UserSchema.statics.create = function(user, callback) {
-  Utils.hashedPassword(user.password).then((hashedPassword) => {
+User.prototype.create = function(user, callback) {
+  Utils.hashedPassword(user.password).then( (hashedPassword) => {
     user.hashedPassword = hashedPassword;
-    user.save(() => {
+    user.save( () => {
       callback();
     });
   }).catch((err) => {
@@ -76,9 +82,9 @@ UserSchema.statics.create = function(user, callback) {
  * @param {string} _id string of ObjectId.
  * @param {function(Error, User)} callback.
 */
-UserSchema.statics.getUserById = function(id, cb) {
+User.prototype.getUserById = function(id, cb) {
   logger.debug(id);
-  this.findOne({_id: new ObjectId(id)}, (err, user) => {
+  this.model.findOne({_id: new ObjectId(id)}, (err, user) => {
     if(err) {
       logger.error(`user authorization ${err}`);
       cb(err);
@@ -94,28 +100,28 @@ UserSchema.statics.getUserById = function(id, cb) {
  * @param {!Object} params with property name and value that will be updated.
  * @param {!function(Error, string)} callback.
 */
-UserSchema.statics.update = function(_id, params, callback) {
+User.prototype.update = function(_id, params, callback) {
  
-  this.findOne({ _id: new ObjectId(_id) }).then((user) => {
+  this.model.findOne({ _id: new ObjectId(_id) }).then((user) => {
     if (user) {
       if (params.hasOwnProperty('newPassword')) {
         
-        Utils.hashedPassword(params.newPassword).then((hashedPassword) => {
+        Utils.hashedPassword(params.newPassword).then( (hashedPassword) => {
           user.hashedPassword = hashedPassword;
-          user.save((err) => {
+          user.save( (err) => {
             if(err) {
               logger.error(`update user error ${err}`);
               callback(err);
             } else callback(null, 'update user success');
           });
-        }).catch((err) => {
+        }).catch( (err) => {
           callback(err);
         });
       } else {
         user.phone = params.phone;
-        user.save().then((res) => {
+        user.save().then( (res) => {
           callback(null, 'update phone success');
-        }).catch((e) => {
+        }).catch( (e) => {
           callback(e);
         });
       }
@@ -126,11 +132,11 @@ UserSchema.statics.update = function(_id, params, callback) {
 
 };
 
-UserSchema.statics.shortNumbers = function(orgCode, cb) {
-  this.findOne({ 'orgnization.code': orgCode }).
-    select('shortNumbers').exec(function(err, res) {
+User.prototype.shortNumbers = function(orgCode, cb) {
+  this.model.findOne({'orgnization.code': orgCode}).
+    select('shortNumbers').exec( (err, res) => {
       cb(err, res);
     });
 };
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = new User();
